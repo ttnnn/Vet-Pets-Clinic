8efbfa99772ccaa19528af437fab4cf7446159b7
Thanaphon <thanaphonjomjindarat@gmail.com>
2025-02-10 15:08:03 +0700

    server render

--- server/server.js
+++ server/server.js
@@ -18,14 +18,13 @@ const PORT = process.env.PORT || 3001;
 pool.connect()
   .then(() => console.log('Connect PostgreSQL Success !!'))
   .catch(err => console.error('การเชื่อมต่อ PostgreSQL ล้มเหลว', err));
-const staticFolder = path.join(__dirname, 'client');
-//ใช้ไฟล์ static สำหรับหน้าลูกค้าและคลินิก
 
-app.use('/customer', express.static(staticFolder, { index: 'index.html' }));
-app.use('/clinic', express.static(staticFolder, { index: 'index.html' }));
-app.use('/public', express.static(path.join(__dirname, '../client/public')));
-app.use('/public', express.static(path.join(__dirname, '../customer/public')));
-
+const staticFolder = path.join(__dirname, '../client/build');
+  app.use(express.static(staticFolder));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(staticFolder, 'index.html'));
+  });
+  
 
   // Routes + เส้นทาง API สำหรับคลินิกและลูกค้า
 app.use('/api/clinic', clinicController);
